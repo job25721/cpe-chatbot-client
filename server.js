5d87dc69d34ce287dcb5686469f51e1db2c1e528
teerapatpor <35361062+teerapatpor@users.noreply.github.com>
2020-03-02 16:59:52 +0700

    GG

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const cors = require('cors');
 const httpStatus = require('http-status');
 const processMessage = require('./process-message');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.wav')
+    }
+})
+
+const upload = multer({ storage: storage })
+
 const app = express();
 
 // Init Middleware
@@ -15,6 +27,10 @@ app.use(cors({ origin: true }));
 app.get('/', (req, res) => {
     res.send('API RUNNING')
 });
+
+app.post('/api/audio-message', upload.single('audio') ,(req,res) => {
+    console.log(req.file.filename);
+})
 
 app.post('/api/dialogflowGateway', async (req, res) => {
     try {
