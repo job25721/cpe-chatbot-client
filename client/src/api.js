7863bddcd4fba2581c4c7815a51a97e16434ebea
job25721 <amashiikaze@gmail.com>
2020-04-12 17:27:26 +0700

    change api url to localhost

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL : 'http://localhost:5000',
-    baseURL : 'https://cpechatbotv1.herokuapp.com/', //production build
+    baseURL : 'http://localhost:5000',
+    // baseURL : 'https://cpechatbotv1.herokuapp.com/', //production build
     withCredentials: false,
     headers: {
         Accept: 'application/json',
